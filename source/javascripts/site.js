5fefb91531ca4d864a2dd6595521404162661891
Aaron A. Rich <aaron@aaronarich.com>
2017-09-21 09:35:51 -0400

    Add decimal rounding to nearest tenth

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,12 @@
 // Round the prices to neartest tenth
+var priceRound = {};
+
+priceRound.round = function(number, precision) {
+    var factor = Math.pow(100, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+};
 
 // $ABB
 $.ajax({
@@ -8,8 +16,8 @@ $.ajax({
   success : function(data) {
       $(".abb-0").text(data.symbol);
       $(".abb-1").text(data.companyName);
-      $(".abb-2").text('$' + data.latestPrice);
-      $(".abb-3").text(data.change);
+      $(".abb-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".abb-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -21,8 +29,8 @@ $.ajax({
   success : function(data) {
       $(".adm-0").text(data.symbol);
       $(".adm-1").text(data.companyName);
-      $(".adm-2").text('$' + data.latestPrice);
-      $(".adm-3").text(data.change);
+      $(".adm-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".adm-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -34,8 +42,8 @@ $.ajax({
   success : function(data) {
       $(".aav-0").text(data.symbol);
       $(".aav-1").text(data.companyName);
-      $(".aav-2").text('$' + data.latestPrice);
-      $(".aav-3").text(data.change);
+      $(".aav-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".aav-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -47,8 +55,8 @@ $.ajax({
   success : function(data) {
       $(".aa-0").text(data.symbol);
       $(".aa-1").text(data.companyName);
-      $(".aa-2").text('$' + data.latestPrice);
-      $(".aa-3").text(data.change);
+      $(".aa-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".aa-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -60,8 +68,8 @@ $.ajax({
   success : function(data) {
       $(".apc-0").text(data.symbol);
       $(".apc-1").text(data.companyName);
-      $(".apc-2").text('$' + data.latestPrice);
-      $(".apc-3").text(data.change);
+      $(".apc-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".apc-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -73,8 +81,8 @@ $.ajax({
   success : function(data) {
       $(".aapl-0").text(data.symbol);
       $(".aapl-1").text(data.companyName);
-      $(".aapl-2").text('$' + data.latestPrice);
-      $(".aapl-3").text(data.change);
+      $(".aapl-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".aapl-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -86,8 +94,8 @@ $.ajax({
   success : function(data) {
       $(".asg-0").text(data.symbol);
       $(".asg-1").text(data.companyName);
-      $(".asg-2").text('$' + data.latestPrice);
-      $(".asg-3").text(data.change);
+      $(".asg-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".asg-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -99,8 +107,8 @@ $.ajax({
   success : function(data) {
       $(".bpt-0").text(data.symbol);
       $(".bpt-1").text(data.companyName);
-      $(".bpt-2").text('$' + data.latestPrice);
-      $(".bpt-3").text(data.change);
+      $(".bpt-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".bpt-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -112,8 +120,8 @@ $.ajax({
   success : function(data) {
       $(".brkb-0").text(data.symbol);
       $(".brkb-1").text(data.companyName);
-      $(".brkb-2").text('$' + data.latestPrice);
-      $(".brkb-3").text(data.change);
+      $(".brkb-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".brkb-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -125,8 +133,8 @@ $.ajax({
   success : function(data) {
       $(".chk-0").text(data.symbol);
       $(".chk-1").text(data.companyName);
-      $(".chk-2").text('$' + data.latestPrice);
-      $(".chk-3").text(data.change);
+      $(".chk-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".chk-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -138,8 +146,8 @@ $.ajax({
   success : function(data) {
       $(".cmcsa-0").text(data.symbol);
       $(".cmcsa-1").text(data.companyName);
-      $(".cmcsa-2").text('$' + data.latestPrice);
-      $(".cmcsa-3").text(data.change);
+      $(".cmcsa-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".cmcsa-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -151,8 +159,8 @@ $.ajax({
   success : function(data) {
       $(".czz-0").text(data.symbol);
       $(".czz-1").text(data.companyName);
-      $(".czz-2").text('$' + data.latestPrice);
-      $(".czz-3").text(data.change);
+      $(".czz-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".czz-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -164,8 +172,8 @@ $.ajax({
   success : function(data) {
       $(".dvmt-0").text(data.symbol);
       $(".dvmt-1").text(data.companyName);
-      $(".dvmt-2").text('$' + data.latestPrice);
-      $(".dvmt-3").text(data.change);
+      $(".dvmt-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".dvmt-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -177,8 +185,8 @@ $.ajax({
   success : function(data) {
       $(".dvn-0").text(data.symbol);
       $(".dvn-1").text(data.companyName);
-      $(".dvn-2").text('$' + data.latestPrice);
-      $(".dvn-3").text(data.change);
+      $(".dvn-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".dvn-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -190,8 +198,8 @@ $.ajax({
   success : function(data) {
       $(".dxc-0").text(data.symbol);
       $(".dxc-1").text(data.companyName);
-      $(".dxc-2").text('$' + data.latestPrice);
-      $(".dxc-3").text(data.change);
+      $(".dxc-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".dxc-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -203,8 +211,8 @@ $.ajax({
   success : function(data) {
       $(".ftr-0").text(data.symbol);
       $(".ftr-1").text(data.companyName);
-      $(".ftr-2").text('$' + data.latestPrice);
-      $(".ftr-3").text(data.change);
+      $(".ftr-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".ftr-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -216,8 +224,8 @@ $.ajax({
   success : function(data) {
       $(".ge-0").text(data.symbol);
       $(".ge-1").text(data.companyName);
-      $(".ge-2").text('$' + data.latestPrice);
-      $(".ge-3").text(data.change);
+      $(".ge-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".ge-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -229,8 +237,8 @@ $.ajax({
   success : function(data) {
       $(".gnw-0").text(data.symbol);
       $(".gnw-1").text(data.companyName);
-      $(".gnw-2").text('$' + data.latestPrice);
-      $(".gnw-3").text(data.change);
+      $(".gnw-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".gnw-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -242,8 +250,8 @@ $.ajax({
   success : function(data) {
       $(".hlt-0").text(data.symbol);
       $(".hlt-1").text(data.companyName);
-      $(".hlt-2").text('$' + data.latestPrice);
-      $(".hlt-3").text(data.change);
+      $(".hlt-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".hlt-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -255,8 +263,8 @@ $.ajax({
   success : function(data) {
       $(".hpe-0").text(data.symbol);
       $(".hpe-1").text(data.companyName);
-      $(".hpe-2").text('$' + data.latestPrice);
-      $(".hpe-3").text(data.change);
+      $(".hpe-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".hpe-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -268,8 +276,8 @@ $.ajax({
   success : function(data) {
       $(".hpq-0").text(data.symbol);
       $(".hpq-1").text(data.companyName);
-      $(".hpq-2").text('$' + data.latestPrice);
-      $(".hpq-3").text(data.change);
+      $(".hpq-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".hpq-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -281,8 +289,8 @@ $.ajax({
   success : function(data) {
       $(".intc-0").text(data.symbol);
       $(".intc-1").text(data.companyName);
-      $(".intc-2").text('$' + data.latestPrice);
-      $(".intc-3").text(data.change);
+      $(".intc-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".intc-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -294,8 +302,8 @@ $.ajax({
   success : function(data) {
       $(".kgc-0").text(data.symbol);
       $(".kgc-1").text(data.companyName);
-      $(".kgc-2").text('$' + data.latestPrice);
-      $(".kgc-3").text(data.change);
+      $(".kgc-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".kgc-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -307,8 +315,8 @@ $.ajax({
   success : function(data) {
       $(".msft-0").text(data.symbol);
       $(".msft-1").text(data.companyName);
-      $(".msft-2").text('$' + data.latestPrice);
-      $(".msft-3").text(data.change);
+      $(".msft-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".msft-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -320,8 +328,8 @@ $.ajax({
   success : function(data) {
       $(".nbr-0").text(data.symbol);
       $(".nbr-1").text(data.companyName);
-      $(".nbr-2").text('$' + data.latestPrice);
-      $(".nbr-3").text(data.change);
+      $(".nbr-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".nbr-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -333,8 +341,8 @@ $.ajax({
   success : function(data) {
       $(".nov-0").text(data.symbol);
       $(".nov-1").text(data.companyName);
-      $(".nov-2").text('$' + data.latestPrice);
-      $(".nov-3").text(data.change);
+      $(".nov-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".nov-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -346,8 +354,8 @@ $.ajax({
   success : function(data) {
       $(".nok-0").text(data.symbol);
       $(".nok-1").text(data.companyName);
-      $(".nok-2").text('$' + data.latestPrice);
-      $(".nok-3").text(data.change);
+      $(".nok-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".nok-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -359,8 +367,8 @@ $.ajax({
   success : function(data) {
       $(".pba-0").text(data.symbol);
       $(".pba-1").text(data.companyName);
-      $(".pba-2").text('$' + data.latestPrice);
-      $(".pba-3").text(data.change);
+      $(".pba-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".pba-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -372,8 +380,8 @@ $.ajax({
   success : function(data) {
       $(".pfe-0").text(data.symbol);
       $(".pfe-1").text(data.companyName);
-      $(".pfe-2").text('$' + data.latestPrice);
-      $(".pfe-3").text(data.change);
+      $(".pfe-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".pfe-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -385,8 +393,8 @@ $.ajax({
   success : function(data) {
       $(".sdr-0").text(data.symbol);
       $(".sdr-1").text(data.companyName);
-      $(".sdr-2").text('$' + data.latestPrice);
-      $(".sdr-3").text(data.change);
+      $(".sdr-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".sdr-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -398,8 +406,8 @@ $.ajax({
   success : function(data) {
       $(".sdt-0").text(data.symbol);
       $(".sdt-1").text(data.companyName);
-      $(".sdt-2").text('$' + data.latestPrice);
-      $(".sdt-3").text(data.change);
+      $(".sdt-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".sdt-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -411,8 +419,8 @@ $.ajax({
   success : function(data) {
       $(".per-0").text(data.symbol);
       $(".per-1").text(data.companyName);
-      $(".per-2").text('$' + data.latestPrice);
-      $(".per-3").text(data.change);
+      $(".per-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".per-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -424,8 +432,8 @@ $.ajax({
   success : function(data) {
       $(".snap-0").text(data.symbol);
       $(".snap-1").text(data.companyName);
-      $(".snap-2").text('$' + data.latestPrice);
-      $(".snap-3").text(data.change);
+      $(".snap-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".snap-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -437,8 +445,8 @@ $.ajax({
   success : function(data) {
       $(".twtr-0").text(data.symbol);
       $(".twtr-1").text(data.companyName);
-      $(".twtr-2").text('$' + data.latestPrice);
-      $(".twtr-3").text(data.change);
+      $(".twtr-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".twtr-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -450,8 +458,8 @@ $.ajax({
   success : function(data) {
       $(".txn-0").text(data.symbol);
       $(".txn-1").text(data.companyName);
-      $(".txn-2").text('$' + data.latestPrice);
-      $(".txn-3").text(data.change);
+      $(".txn-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".txn-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -463,8 +471,8 @@ $.ajax({
   success : function(data) {
       $(".vz-0").text(data.symbol);
       $(".vz-1").text(data.companyName);
-      $(".vz-2").text('$' + data.latestPrice);
-      $(".vz-3").text(data.change);
+      $(".vz-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".vz-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -476,8 +484,8 @@ $.ajax({
   success : function(data) {
       $(".win-0").text(data.symbol);
       $(".win-1").text(data.companyName);
-      $(".win-2").text('$' + data.latestPrice);
-      $(".win-3").text(data.change);
+      $(".win-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".win-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -489,8 +497,8 @@ $.ajax({
   success : function(data) {
       $(".wmt-0").text(data.symbol);
       $(".wmt-1").text(data.companyName);
-      $(".wmt-2").text('$' + data.latestPrice);
-      $(".wmt-3").text(data.change);
+      $(".wmt-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".wmt-3").text(priceRound.round(data.change, 1));
   },
 });
 
@@ -502,7 +510,7 @@ $.ajax({
   success : function(data) {
       $(".znga-0").text(data.symbol);
       $(".znga-1").text(data.companyName);
-      $(".znga-2").text('$' + data.latestPrice);
-      $(".znga-3").text(data.change);
+      $(".znga-2").text('$' + priceRound.round(data.latestPrice, 1));
+      $(".znga-3").text(priceRound.round(data.change, 1));
   },
 });
